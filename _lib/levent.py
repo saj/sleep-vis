40c00095bb4852fe0e4f446d7a50b9373ea239cd
Saj Goonatilleke <sg@redu.cx>
2022-09-11 02:35:28 +1000

    merge adjacent events in duration-by-wake-weekday, too (bug)
    Originally from commit 838cc40.
    This transformation is required in any analysis that is sensitive to
    individual event duration.

--- _lib/levent.py
+++ _lib/levent.py
@@ -1,3 +1,4 @@
+import collections
 import datetime
 import json
 import typing
@@ -48,6 +49,53 @@ def parse_datetime(stamp):
 
     """
     return datetime.datetime.strptime(stamp, PARSE_SAA_TIME_FORMAT)
+
+
+def merge_adjoining(it, closeness=datetime.timedelta(hours=4)):
+    """Merge adjoining events from iterator it.
+
+    Two events, a and b, are adjoining if b starts when a ends.
+    Iterator it must yield events in ascending chronological order.
+    """
+    # How do adjoining events occur in the dataset?
+    # I suspect they are an artefact of the following real-world pattern:
+    #
+    #   An alarm is set to activate after ~8h of sleep.
+    #   I go to sleep.
+    #   ~8h later, the alarm activates, and is dismissed.  (No snooze.)
+    #   -> The first event is recorded.
+    #   I sleep some more. :)
+    #   Later, after waking naturally without an alarm,
+    #   I add a new sleep event to record the additional time spent asleep.
+    #   -> The second event is recorded.
+    #
+    # Such events are almost always perfectly adjoining.
+    # The first event ends on the same minute the next event starts.
+    #
+    # Events are not always perfectly adjoining -- for reasons unknown.
+    # We merge an event with its preceding event if they are only separated by
+    # a small number of hours.  closeness defines the merge threshold.
+
+    adj = collections.deque()
+    for next_event in it:
+        if not adj:
+            adj.append(next_event)
+            continue
+
+        last_event = adj[-1]
+        if next_event.since - last_event.until <= closeness:
+            adj.append(next_event)
+            continue
+
+        # last_event not adjacent to next_event; flush
+        yield Event(since=adj[0].since, until=adj[-1].until)
+        adj.clear()
+        adj.append(next_event)
+        continue
+
+    if adj:
+        yield Event(since=adj[0].since, until=adj[-1].until)
+        adj.clear()
 
 
 if __name__ == "__main__":
